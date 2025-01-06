051f6c524b292cdda64546144d7c1d852092c8b1
v4vikram <v4vikram.dev@gmail.com>
2025-01-06 16:52:10 +0530

    backend setup

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+app.use(cors({ origin: '*' }));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+module.exports = app;
+
